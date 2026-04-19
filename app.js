f4b06d1fb75f3839266d00a7fab28f79934cc0e7
Ronald Ponce <geosist5@gmail.com>
2026-04-19 17:27:25 -0500

    primer commit agregamos los archivos del parcial

---
+++ app.js
@@ -0,0 +1,30 @@
+import { suma } from './funciones/suma.js';
+import { promedio } from './funciones/promedio.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  document.getElementById("btnCalcular").addEventListener("click", calcular);
+
+});
+
+function calcular() {
+
+  const op = document.getElementById("operacion").value;
+  const v1 = parseFloat(document.getElementById("valor1").value);
+  const v2 = parseFloat(document.getElementById("valor2").value);
+
+  if (isNaN(v1) || isNaN(v2)) {
+    alert("Ingrese valores válidos");
+    return;
+  }
+
+  let resultado;
+
+  if (op === "suma") {
+    resultado = suma(v1, v2);
+  } else if (op === "promedio") {
+    resultado = promedio(v1, v2);
+  }
+
+  document.getElementById("resultado").innerText = "Resultado: " + resultado;
+}
