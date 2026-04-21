5c88397a84f4a51c2d29900fd8bd59d8120de802
TinuFagbemi <fagbemitinuola@gmail.com>
2026-04-20 19:00:32 -0500

    G3: Agrega función máximo con 2 inputs – Tinuola-y-Javier

---
+++ funciones/maximo.js
@@ -0,0 +1,12 @@
+// funciones/maximo.js
+// Función: máximo
+// Grupo: G3
+// Estudiantes: Tinuola Fagbemi y Javier Díaz
+// Descripción: Recibe dos números y retorna el mayor de ambos.
+
+export function maximo(a, b) {
+  if (a >= b) {
+    return a;
+  }
+  return b;
+}
